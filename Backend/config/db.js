272849cb0025fc14c60da28d43f8b6b6830064d2
Abhishek Ray <ab7731929@gmail.com>
2024-08-15 13:16:43 +0530

    first-commit

---
+++ Backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+export const connectDB = async () => {
+    await mongoose.connect(process.env.DB_URL).then(()=>console.log("DataBase Connected Successfully!"));
+}
